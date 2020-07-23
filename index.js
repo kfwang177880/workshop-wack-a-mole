147ed47df23a071aa61d25e33bb4d66dda0fbcdd
kfwang177880 <kfwang177880@yahoo.com>
2020-07-23 17:45:08 -0400

    modified the interval

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
 
 let score = 0;
 var scoreDisplay = document.getElementById('score');
+var holes = document.getElementsByClassName('hole');
+setInterval(function(){
+  var randomHoleIndex = Math.floor(Math.random() * holes.length);
+  holes[randomHoleIndex].classList.toggle('mole');
+}, 500);
 
-console.log(scoreDisplay);
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent) {
+    if (clickEvent.target.matches('.mole')) {
+      clickEvent.target.classList.remove('mole')
+      score++
+      scoreDisplay.innerText=score;
+    }
+} );
+
+
